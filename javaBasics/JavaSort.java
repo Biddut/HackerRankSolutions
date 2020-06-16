e94af198b9f3c7614bb110527e7de154a42ddf3e
vidyut07 <vidyut07@gmail.com>
2020-06-15 23:24:36 -0400

    HourGlass

---
+++ javaBasics/JavaSort.java
@@ -0,0 +1,58 @@
+package javaBasics;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+class Student{
+	private int id;
+	private String fname;
+	private double cgpa;
+	public Student(int id, String fname, double cgpa) {
+		super();
+		this.id = id;
+		this.fname = fname;
+		this.cgpa = cgpa;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public double getCgpa() {
+		return cgpa;
+	}
+}
+
+class NameComparator implements Comparator<Student>{  
+	public int compare(Student s1,Student s2){  
+		return s1.getFname().compareTo(s2.getFname());  
+	}  
+}
+public class JavaSort
+{
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		
+		List<Student> studentList = new ArrayList<Student>();
+		while(testCases>0){
+			int id = in.nextInt();
+			String fname = in.next();
+			double cgpa = in.nextDouble();
+			
+			Student st = new Student(id, fname, cgpa);
+			studentList.add(st);
+			
+			testCases--;
+		}
+		Collections.sort(studentList,new NameComparator());  
+      
+      	for(Student st: studentList){
+			System.out.println(st.getFname());
+		}
+	}
+}
