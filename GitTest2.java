d7da7e01e04f5873f024861a3cbf0cffb7fd52d7
vidyut07 <vidyut07@gmail.com>
2020-09-27 03:45:15 -0400

    Git Commit 2nd file

---
+++ GitTest2.java
@@ -0,0 +1,11 @@
+
+public class GitTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello GIT1");
+		System.out.println("git add * or add .");
+
+	}
+
+}
