8dcd97d85db707fa7ca58ad73a66c186249f1537
vidyut07 <vidyut07@gmail.com>
2020-09-27 03:10:12 -0400

    Revert "Git commit without staging" revert the changes
    This reverts commit a33a73dc716d1ac0456d3d829fa8a68ec82cefea.

--- GitTest1.java
+++ GitTest1.java
@@ -4,8 +4,6 @@ public class GitTest1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello GIT1");
-		
-		System.out.println("Hello GIT 2");
 
 	}
 
