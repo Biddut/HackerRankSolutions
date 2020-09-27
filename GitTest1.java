d5ff12ceb3d18cf3703ce9fa7347de6940180ad2
vidyut07 <vidyut07@gmail.com>
2020-09-27 03:36:18 -0400

    Git chekout test

--- GitTest1.java
+++ GitTest1.java
@@ -4,6 +4,7 @@ public class GitTest1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello GIT1");
+		System.out.println("git add * or add .");
 
 	}
 
